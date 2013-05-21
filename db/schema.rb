f636f364c745519a4d32dc24af93332777db5a25
manishkukreja <manishkukreja@gmail.com>
2013-05-21 09:38:20 +0530

    jquery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121216061217) do
+ActiveRecord::Schema.define(:version => 20130415131156417) do
 
   create_table "books", :force => true do |t|
     t.string   "name"
@@ -72,6 +72,45 @@ ActiveRecord::Schema.define(:version => 20121216061217) do
     t.datetime "updated_at"
   end
 
+  create_table "line_items", :force => true do |t|
+    t.integer  "book_id"
+    t.integer  "reading_list_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "rates", :force => true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "stars",         :null => false
+    t.string   "dimension"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  add_index "rates", ["rateable_id", "rateable_type"], :name => "index_rates_on_rateable_id_and_rateable_type"
+  add_index "rates", ["rater_id"], :name => "index_rates_on_rater_id"
+
+  create_table "rating_caches", :force => true do |t|
+    t.integer  "cacheable_id"
+    t.string   "cacheable_type"
+    t.float    "avg",            :null => false
+    t.integer  "qty",            :null => false
+    t.string   "dimension"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  add_index "rating_caches", ["cacheable_id", "cacheable_type"], :name => "index_rating_caches_on_cacheable_id_and_cacheable_type"
+
+  create_table "reading_lists", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "taggings", :force => true do |t|
     t.integer  "book_id"
     t.integer  "tag_id"
@@ -90,6 +129,16 @@ ActiveRecord::Schema.define(:version => 20121216061217) do
     t.integer  "language_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "user_invitees", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "friend_name"
+    t.string   "friend_email"
+    t.date     "invitation_date"
+    t.string   "invitation_status"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
   end
 
   create_table "user_sessions", :force => true do |t|
