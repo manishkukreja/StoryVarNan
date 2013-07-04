b205274c60db6c6e9a5bc9afa700bcb200715396
manishkukreja <manishkukreja@gmail.com>
2013-07-05 00:09:40 +0530

    delete content for remove column from user migration

--- db/migrate/20130704023206_remove_column_from_user.rb
+++ db/migrate/20130704023206_remove_column_from_user.rb
@@ -1,11 +1,3 @@
 class RemoveColumnFromUser < ActiveRecord::Migration
-  def up
-    remove_column :users, :provider
-    remove_column :users, :uid
-  end
-
-  def down
-    add_column :users, :uid, :string
-    add_column :users, :provider, :string
-  end
+ 
 end
