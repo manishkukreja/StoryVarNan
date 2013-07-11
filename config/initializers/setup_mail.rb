b236040cb05a8293b40b9775edfd56ee1a792c14
Manish <manishkukreja@gmail.com>
2013-07-11 20:22:41 +0530

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "manishkukreja",
-  :password             => "snowy@2012",
+  :password             => "########",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
