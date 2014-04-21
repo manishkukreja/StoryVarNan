760e33d8b14b68819284555355a67abc16cafc3c
Manish <manishkukreja@gmail.com>
2014-04-21 16:56:58 +0530

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: "AKIAICKOZWK3QEYOYLVQ",
-    aws_secret_access_key: "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"
+    aws_access_key_id: "removed access id",
+    aws_secret_access_key: "removed_access key"
   }
   config.fog_directory = "storyvarnan_data"                    # required
   config.fog_public     = true                                 # optional, defaults to true
